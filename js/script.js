67120079008ec5b68e71977744dc9410e2674290
LucasRossatto <154370022+LucasRossatto@users.noreply.github.com>
2024-08-09 13:55:56 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//query selector para selecionar como consts os elementos utilizados nos eventos
 const nome = document.querySelector("#nome");
 const idade = document.querySelector("#idade");
 const telefone = document.querySelector("#telefone");
@@ -16,54 +15,21 @@ btn.addEventListener("click", (e) => {
   //Const que guarda a estrutura da div que exibira as informações cadastradas
   const InputsLi = `
   <div class="list-item">
-<<<<<<< HEAD
-    Nome: ${inputNameValue}<br> 
-    Telefone: ${inputTelefoneValue}<br> 
-    Idade: ${inputIdadeValue}<br>
-=======
     Nome: ${inputNameValue} 
     Telefone: ${inputTelefoneValue} 
     Idade: ${inputIdadeValue}
->>>>>>> 69da1302971b8ce0d11c37ba717421c459786719
       <button class="DeleteBtn" type="button">Deletar</button>
   </div>
 `;
 
-<<<<<<< HEAD
-
-    // Validando valores nulos
-    if(inputNameValue ===""){
-        alert("Digite alguma coisa")
-        return false;
-    }
-    else if(inputIdadeValue === ""){
-        alert("Digite alguma coisa")
-        return false;
-    } else if(inputTelefoneValue === ""){
-=======
 //escrevendo a InputsLi no html
-lista.innerHTML += InputsLi;
 
 
     if(inputNameValue == ""){
->>>>>>> 69da1302971b8ce0d11c37ba717421c459786719
         alert("Digite alguma coisa")
         return false;
     }
 
-<<<<<<< HEAD
-    // escrevendo a InputsLi no html
-    lista.innerHTML += InputsLi;
-
-
-    nome.value = "";
-    idade.value = "";
-    telefone.value = "";
-
-    lista.innerHTML += InputsLi + DeleteBtn ;
-   
-    
-=======
     if(inputIdadeValue == ""){
         alert("Digite alguma coisa")
         return false;
@@ -72,13 +38,13 @@ lista.innerHTML += InputsLi;
         alert("Digite alguma coisa")
         return false;
     }
+    lista.innerHTML += InputsLi;
 
     lista.innerHTML += InputsLi + DeleteBtn ;
-   
+
     nome.value = "";
     idade.value = "";
     telefone.value = "";
->>>>>>> 69da1302971b8ce0d11c37ba717421c459786719
 });
 
 lista.addEventListener("click", (e) => {
